9545ab6d6e3cbd9a422b51bf111beed4f53f26d6
Raushan225024 <raushankumar225024@gmail.com>
2026-09-10 23:04:09 +0530

    mew commit

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -2,14 +2,18 @@ from langchain_qdrant import QdrantVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 import config.config as config
 
+print("STEP 1: retriever.py imported")
 
-# Load model only ONCE when the application starts
 embedding = HuggingFaceEmbeddings(
     model_name=config.EMBEDDING_MODEL
 )
 
+print("STEP 2: embedding model loaded")
+
 
 def retrieve_documents(query, k=6):
+
+    print("STEP 3: retrieving documents")
 
     vector_store = QdrantVectorStore.from_existing_collection(
         path=config.VECTOR_DB_DIR,
@@ -17,10 +21,10 @@ def retrieve_documents(query, k=6):
         embedding=embedding,
     )
 
+    print("STEP 4: vector store loaded")
+
     retriever = vector_store.as_retriever(
         search_kwargs={"k": k}
     )
 
-    docs = retriever.invoke(query)
-
-    return docs
+    return retriever.invoke(query)
